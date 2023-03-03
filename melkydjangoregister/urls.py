a221f74cca3cb1d3b089b564971aacec5ba374c6
eustace17 <melklem20@gmail.com>
2023-03-03 13:46:14 +0300

    added add products option

--- melkydjangoregister/urls.py
+++ melkydjangoregister/urls.py
@@ -23,6 +23,7 @@ urlpatterns = [
     path('', general_views.register, name = 'register'),
     path('login/', auth_views.LoginView.as_view(template_name = 'login.html'), name = 'login'),
     path('home/', general_views.home, name='home'),
-    path('logout/', auth_views.LogoutView.as_view(template_name='logout.html'), name='logout')
+    path('logout/', auth_views.LogoutView.as_view(template_name='logout.html'), name='logout'),
+    path ('add-product/',general_views.add_product ,name = 'add-product')
 
 ]
--- melkydjangoregister/views.py
+++ melkydjangoregister/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from .forms import UserRegistrationForm
 from django.contrib.auth.decorators import login_required
+from .models import Product
 
 def register(request) :
     if  request.method == "POST" :
@@ -18,3 +19,22 @@ def register(request) :
 @login_required
 def home (request) :
     return render (request,'home.html')
+
+@login_required
+def add_product (request) :
+    #check if form submitted has a method post
+    if request.method == 'POST' :
+        #start receiving data from the form
+        p_name = request.POST.get('jina')
+        p_quantity = request.POST.get('kiasi')
+        p_price = request.POST. get ('bei')
+
+        #finally save data in our table called products
+        product = Product(prod_name = p_name, prod_quantity=p_quantity,prod_price=p_price)
+
+        product.save()
+
+        #Redirect back with a success message
+        messages.success(request, 'Product Saved Successfully')
+        return redirect('add-product')
+    return render(request,'addproducts.html')
